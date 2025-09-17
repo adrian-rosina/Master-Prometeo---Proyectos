e9c483c27c68ec27e195912abd45ef96a10314e7
adrian.rosina <adrianrosinacr@gmail.com>
2025-09-17 03:23:40 +0200

    Añado corrección en ejercicio 42

--- proyecto2-katas-js/ejercicios-basicos-avanzados/ejercicio-42.js
+++ proyecto2-katas-js/ejercicios-basicos-avanzados/ejercicio-42.js
@@ -1,5 +1,5 @@
 function swap(array, index1, index2) {
-    // Intercambiamos usando destructuring
+    
     [array[index1], array[index2]] = [array[index2], array[index1]];
     return array;
   }
